3f29efa0aacaf9df70ef4463394036adbe2900a7
Austin Winstanley <austin@axialcommerce.com>
2015-10-19 13:08:39 -0500

    added nuspec

--- src/Hangfire.Raven/Properties/AssemblyInfo.cs
+++ src/Hangfire.Raven/Properties/AssemblyInfo.cs
@@ -2,10 +2,16 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Hangfire.Raven")]
+[assembly: AssemblyTitle("Hangfire Raven")]
 [assembly: AssemblyDescription("Raven job storage for Hangfire")]
 [assembly: Guid("3d96bf2f-8854-4872-aee3-faf81d121a4d")]
 
 [assembly: InternalsVisibleTo("Hangfire.Raven.Tests")]
 // Allow the generation of mocks for internal types
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+[assembly: AssemblyCompany("Axial Commerce")]
+[assembly: AssemblyProduct("Hangfire Raven")]
+[assembly: AssemblyCopyright("Copyright © Axial Commerce")]
+[assembly: AssemblyVersion("1.7")]
+[assembly: AssemblyFileVersion("1.7")]
+
