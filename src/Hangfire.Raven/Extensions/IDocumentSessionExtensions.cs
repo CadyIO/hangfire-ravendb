35cb835a6acf3f95ca6660bef09436d38cff21dd
adsamcik <adsamcik@users.noreply.github.com>
2018-01-24 09:53:56 +0100

    Improved IDocumentSessionExtensions

--- src/Hangfire.Raven/Extensions/IDocumentSessionExtensions.cs
+++ src/Hangfire.Raven/Extensions/IDocumentSessionExtensions.cs
@@ -8,8 +8,15 @@ namespace Hangfire.Raven.Extensions {
     public static class IDocumentSessionExtensions {
         private static IMetadataDictionary GetMetadata<T>(this IDocumentSession session, string id) => session.Advanced.GetMetadataFor(session.Load<T>(id));
 
+        private static IMetadataDictionary GetMetadata<T>(this IDocumentSession session, T obj) => session.Advanced.GetMetadataFor(obj);
+
         public static void SetExpiry<T>(this IDocumentSession session, string id, TimeSpan expireIn) {
             var metadata = session.GetMetadata<T>(id);
+            metadata[Constants.Documents.Metadata.Expires] = (DateTime.UtcNow + expireIn).ToString("O");
+        }
+
+        public static void SetExpiry<T>(this IDocumentSession session, T obj, TimeSpan expireIn) {
+            var metadata = session.GetMetadata(obj);
             metadata[Constants.Documents.Metadata.Expires] = (DateTime.UtcNow + expireIn).ToString("O");
         }
 
@@ -27,7 +34,7 @@ namespace Hangfire.Raven.Extensions {
         }
 
         public static DateTime GetExpiry<T>(this IDocumentSession session, T obj) {
-            var metadata = session.Advanced.GetMetadataFor(obj);
+            var metadata = GetMetadata(obj);
             if (metadata.ContainsKey(Constants.Documents.Metadata.Expires))
                 return (DateTime)metadata[Constants.Documents.Metadata.Expires];
             else
